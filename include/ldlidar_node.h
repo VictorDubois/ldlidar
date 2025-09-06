c045b24c3387fc04c4e4ebf297e999b92597d349
Victor Dubois <victor.dubois@navya.tech>
2025-09-06 17:41:27 +0200

    clangFormat

--- include/ldlidar_node.h
+++ include/ldlidar_node.h
@@ -1,28 +1,28 @@
 #ifndef LD06_H
 #define LD06_H
 
-#include <iostream>
 #include "cmd_interface_linux.h"
-#include <stdio.h>
 #include "lipkg.h"
-#include <rclcpp/rclcpp.hpp>
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "tofbf.h"
-#include <string>
 #include <diagnostic_updater/diagnostic_updater.hpp>
+#include <iostream>
+#include <rclcpp/rclcpp.hpp>
+#include <stdio.h>
+#include <string>
 
 using namespace std::chrono_literals;
 
 class LD06 : public rclcpp::Node
 {
-  public:
+public:
     LD06();
     void produce_diagnostics(diagnostic_updater::DiagnosticStatusWrapper& stat);
 
-  private:
+private:
     CmdInterfaceLinux cmd_port_;
     rclcpp::TimerBase::SharedPtr loop_timer_;
-    LiPkg * lidar_;
+    LiPkg* lidar_;
     rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr lidar_pub_;
     void publishLoop();
     bool m_cannot_find_lidar = false;
